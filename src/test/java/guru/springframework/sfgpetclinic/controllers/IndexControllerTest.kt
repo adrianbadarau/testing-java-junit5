6ca905fa26bc3d25453076786b31c8a9a659d6cf
Adrian Badarau <abadarau@lenovo.com>
2019-04-02 15:35:58 +0200

    implemented first tests in kotlin

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.kt
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.kt
@@ -6,16 +6,20 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class IndexControllerTest {
-
+    val indexController = IndexController()
     @BeforeEach
     fun setUp() {
+
     }
 
     @Test
     fun index() {
+        assertEquals("index", indexController.index())
+        assertNotEquals("indexd", indexController.index(), "Wrong view returned")
     }
 
     @Test
     fun oupsHandler() {
+        assertTrue("notimplemented".equals(indexController.oupsHandler()))
     }
 }

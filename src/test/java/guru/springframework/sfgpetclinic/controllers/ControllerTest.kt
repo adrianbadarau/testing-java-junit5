370e67d9677754ba83f3cd1d4271fab8fce47d96
Adrian Badarau <abadarau@lenovo.com>
2019-04-02 20:08:06 +0200

    added jvm default to kotlin  and default methods to the interface

--- src/test/java/guru/springframework/sfgpetclinic/controllers/ControllerTest.kt
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/ControllerTest.kt
@@ -1,7 +1,14 @@
 package guru.springframework.sfgpetclinic.controllers
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Tag
 
 @Tag("controllers")
 interface ControllerTest {
+
+    @JvmDefault
+    @BeforeEach
+    fun beforeAll(){
+        print("Running from the interface")
+    }
 }

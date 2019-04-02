d3e961af3fcbe2d4023fbc22523532ca8b75e3fb
Adrian Badarau <abadarau@lenovo.com>
2019-04-02 16:51:09 +0200

    implemented conditional runs lesson in kotlin

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.kt
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.kt
@@ -5,15 +5,15 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Assumptions.assumeTrue
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.condition.*
 import java.time.Duration
 
 internal class IndexControllerTest {
     val indexController = IndexController()
-    @BeforeEach
-    fun setUp() {
+    val userName = "abadarau"
 
-    }
 
     @Test
     @DisplayName("Test proper view name is returned for the index page")
@@ -46,5 +46,35 @@ internal class IndexControllerTest {
     @Test
     internal fun testAssumptionTrue() {
         assumeTrue("GURU".equals("guru",true))
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    fun testRunOnlyOnWindows(){
+
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    fun testRunOnlyOnLinux(){
+
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_8)
+    fun testRunOnJdk8(){
+
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_11)
+    fun testRunOnJdk11(){
+
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "JAVA_HOME",matches = ".+")
+    fun testRunOnlyIfJavaHomeIsSet(){
+
     }
 }

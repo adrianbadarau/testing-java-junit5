cee6ed2ed3443786ed8db64b31f3b216e7873cb4
Adrian Badarau <abadarau@lenovo.com>
2019-04-02 15:43:56 +0200

    implemented group tests lesson in kotlin

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.kt
@@ -0,0 +1,19 @@
+package guru.springframework.sfgpetclinic.model
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+internal class PersonTest {
+    @Test
+    internal fun groupedAssertions() {
+        //given
+        val person = Person(1L, "Joe", "Buck")
+        //then
+        assertAll("TestPropsSet",
+                { assertEquals(person.firstName, "Joe") },
+                { assertEquals(person.lastName, "Buck") }
+        )
+
+    }
+}

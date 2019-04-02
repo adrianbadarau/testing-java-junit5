cb66964d934b91c121dc0d68b5698b319fbce74e
Adrian Badarau <abadarau@lenovo.com>
2019-04-02 15:53:44 +0200

    implemented dependent assertions lesson in kotlin

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.kt
@@ -0,0 +1,30 @@
+package guru.springframework.sfgpetclinic.model
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+internal class OwnerTest {
+
+    @Test
+    fun dependentAssertions() {
+        val owner = Owner(1L, "Joe", "Buck")
+        owner.city = "Key West"
+        owner.telephone = "1231231234"
+
+        assertAll("Properties Test",
+                {
+                    assertAll("Person Props",
+                            { assertEquals("Joe", owner.firstName) },
+                            { assertEquals("Buck", owner.lastName) }
+                    )
+                },
+                {
+                    assertAll("Owner Props",
+                            { assertEquals("Key West", owner.city) },
+                            { assertEquals("1231231234", owner.telephone) }
+                    )
+                }
+        )
+    }
+}

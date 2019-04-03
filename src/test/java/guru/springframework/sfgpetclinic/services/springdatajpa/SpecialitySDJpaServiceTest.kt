95f9c09f14812cf66a9cd37b50c150c677ba8a2e
Adrian Badarau <abadarau@lenovo.com>
2019-04-03 14:55:56 +0200

    created mockito with return values

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.kt
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.kt
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.services.springdatajpa
 
 import guru.springframework.sfgpetclinic.model.Speciality
 import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -10,6 +12,7 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.mockito.junit.jupiter.MockitoExtension
+import java.util.*
 
 @ExtendWith(MockitoExtension::class)
 internal class SpecialitySDJpaServiceTest {
@@ -18,7 +21,7 @@ internal class SpecialitySDJpaServiceTest {
     lateinit var specialtyRepository: SpecialtyRepository
 
     @InjectMocks
-    lateinit var service:SpecialitySDJpaService
+    lateinit var service: SpecialitySDJpaService
 
     @Test
     fun deleteById() {
@@ -29,7 +32,16 @@ internal class SpecialitySDJpaServiceTest {
     }
 
     @Test
-    fun delete(){
+    fun delete() {
         service.delete(Speciality())
+    }
+
+    @Test
+    fun findByIdTest() {
+        val speciality = Speciality()
+        `when`(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality))
+        val found = service.findById(1L)
+        assertThat(found).isNotNull
+        verify(specialtyRepository).findById(1L)
     }
 }

939a655e4b15e0b6c47f2cc3aa45f3c58faff0d0
Adrian Badarau <abadarau@lenovo.com>
2019-04-03 10:58:47 +0200

    added a mock integration test

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.kt
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class PetSDJpaServiceIT {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @Test
+    fun findAll() {
+    }
+
+    @Test
+    fun findById() {
+    }
+
+    @Test
+    fun save() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun deleteById() {
+    }
+}
